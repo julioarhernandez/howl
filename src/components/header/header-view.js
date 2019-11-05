80f41a89f993bfceab8547f3a0242622b039fef6
Julio Rodriguez <Juliorodriguez@ncl.com>
2019-11-05 17:33:55 -0500

    add material UI components

--- src/components/header/header-view.js
+++ src/components/header/header-view.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 import './header-styles';
 
+const useStyles = makeStyles(theme => ({
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center',
+    },
+}));
+
 const Header = ({title}) => {
+    const classes = useStyles();
     return ( 
-        <h1>{title}</h1>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" color="inherit" className={classes.menuButton} aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    {title}
+                </Typography>
+                <Button color="inherit">{title}</Button>
+            </Toolbar>
+        </AppBar>
      );
 }
 export default Header;
