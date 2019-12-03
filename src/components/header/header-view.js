c49a5a7bcaea24e0b2e3cf961f00b8b20149d4cd
Julio Rodriguez <Juliorodriguez@ncl.com>
2019-12-03 17:26:05 -0500

    Header back button

--- src/components/header/header-view.js
+++ src/components/header/header-view.js
@@ -4,12 +4,13 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Badge from '@material-ui/core/Badge';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import TimerIcon from '@material-ui/icons/Timer';
 import MenuIcon from '@material-ui/icons/Menu';
 import useStyles from './header-styles';
 
-const Header = ({title, counter}) => {
+const Header = ({title, counter, backLink}) => {
     const classes = useStyles();
     return ( 
         <AppBar position="static">
@@ -17,13 +18,16 @@ const Header = ({title, counter}) => {
                 <IconButton edge="start" color="inherit" className={classes.menuButton} aria-label="menu">
                     <MenuIcon />
                 </IconButton>
-                <Typography variant="h6" className={classes.title}>
+                <Typography variant="h6" className={backLink ? classes.titleLeft : classes.titleCenter}>
                     {title}
                 </Typography>
                 {counter &&
                     <Badge color="secondary" badgeContent={counter}>
                         <TimerIcon />
                     </Badge>
+                }
+                {backLink &&
+                    <Button color="inherit" className={classes.title}>{backLink}</Button>
                 }
             </Toolbar>
         </AppBar>
@@ -32,6 +36,7 @@ const Header = ({title, counter}) => {
 
 Component.PropTypes = {
     title: PropTypes.string.isRequired,
-    counter: PropTypes.string
+    counter: PropTypes.string,
+    backLink: PropTypes.string
 };
 export default Header;
--- src/components/header/header.stories.js
+++ src/components/header/header.stories.js
@@ -10,10 +10,14 @@ export default {
 
 export const HeaderText = () => <Header title={text("Title", "Howl")} counter={text("counter", "2")} onClick={action('Clicked')}/>;
 export const HeaderText2 = () => <Header title={text("Title", "Howl")} onClick={action('Clicked')}/>;
+export const HeaderText3 = () => <Header title={text("Title", "Howl")} backLink={text("Back", "Back")} onClick={action('Clicked')}/>;
 
 HeaderText.story = {
     name: 'Header with counter',
 };
 HeaderText2.story = {
     name: 'Header w/o Counter',
+};
+HeaderText3.story = {
+    name: 'Header w/ Back Link',
 };
