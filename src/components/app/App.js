490ab5f88c9e4ac0935803bd0d2e1a2501f909bf
Julio Rodriguez <Juliorodriguez@ncl.com>
2019-12-03 17:26:05 -0500

    Addicon button added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { makeStyles} from '@material-ui/core/styles';
 import Header from '../header';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 import ItemList from '../itemList';
 import './App.css';
 
-
+const appStyles = makeStyles(theme => ({
+    fab: {
+        position: 'absolute',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+        color: 'secondary'
+    }
+}));
 const App = () => {
+    const classes = appStyles();
     const itemArray = [
         {
             title: "Lorem Ipsum name product",
@@ -23,6 +34,10 @@ const App = () => {
         <>
             <Header title="Howl" counter="2"/>
             <ItemList items={itemArray}/>
+            <Fab aria-label="Add" className={classes.fab} color="secondary">
+                <AddIcon />
+            </Fab>
+            
         </>
     );
 }
